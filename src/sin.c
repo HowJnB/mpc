b19bbc26f1c7e289dea8b314e2241c67c1bc8462
Philippe Theveny <philippe.theveny@laposte.net>
2008-04-21 15:29:37 +0000

    Add special values processing in mpc_sin as in the C99 standard
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@115 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- src/sin.c
+++ src/sin.c
@@ -31,20 +31,61 @@ mpc_sin (mpc_ptr rop, mpc_srcptr op, mpc_rnd_t rnd)
   mp_prec_t prec;
   int ok = 0;
 
-  /* let op = a + i*b, then sin(op) = sin(a)*cosh(b) + i*cos(a)*sinh(b).
+  /* special values */
+  if (!mpfr_number_p (MPC_RE (op)) || !mpfr_number_p (MPC_IM (op)))
+    {
+      if (mpfr_nan_p (MPC_RE (op)) || mpfr_nan_p (MPC_IM (op)))
+        {
+          mpc_set (rop, op, rnd);
 
-     We use the following algorithm (same for the imaginary part),
-     with rounding to nearest for all operations, and working precision w:
+          if (mpfr_nan_p (MPC_IM (op)))
+            /* OP = x +i NaN, then ROP = NaN +i*NaN except for x=0 */
+            {
+              if (!mpfr_zero_p (MPC_RE (op)))
+                mpfr_set_nan (MPC_RE (rop));
+              return;
+            }
 
-     (1) x = o(sin(a))
-     (2) y = o(cosh(b))
-     (3) r = o(x*y)     
-     then the error on r is at most 4 ulps, since we can write
-     r = sin(a)*cosh(b)*(1+t)^3 with |t| <= 2^(-w),
-     thus for w >= 2, r = sin(a)*cosh(b)*(1+4*t) with |t| <= 2^(-w),
-     thus the relative error is bounded by 4*2^(-w) <= 4*ulp(r).
-  */
-  
+          /* OP = NaN +i*y, then ROP = NaN +i*NaN except if x=0 or infinity */
+          if (!mpfr_inf_p (MPC_IM (op)) && !mpfr_zero_p (MPC_IM (op)))
+            mpfr_set_nan (MPC_IM (rop));
+
+          return;
+        }
+
+      if (mpfr_inf_p (MPC_RE (op)))
+        /* OP = infinity +i*y where y is not NaN */
+        {
+          mpfr_set_nan (MPC_RE (rop));
+
+          if (!mpfr_inf_p (MPC_IM (op)) && !mpfr_zero_p (MPC_IM (op)))
+            mpfr_set_nan (MPC_IM (rop));
+          else
+            mpfr_set (MPC_IM (rop), MPC_IM (op), MPC_RND_IM (rnd));
+
+          return;
+        }
+
+      /* OP = x +i*infinity where x is finite */
+      if (mpfr_zero_p (MPC_RE (op)))
+        {
+          mpc_set (rop, op, rnd);
+
+          return;
+        }
+
+      /* ROP = infinity*(sin x -i*cos x) */
+      mpfr_init (x);
+      mpfr_init (y);
+      mpfr_sin_cos (x, y, MPC_RE (op), GMP_RNDZ);
+      mpfr_set_inf (MPC_RE (rop), mpfr_sgn (x));
+      mpfr_set_inf (MPC_IM (rop), mpfr_sgn (y));
+      mpfr_clear (y);
+      mpfr_clear(x);
+
+      return;
+    }
+
   /* special case when the input is real: sin(x) = sin(x) */
   if (mpfr_cmp_ui (MPC_IM(op), 0) == 0)
     {
@@ -61,6 +102,20 @@ mpc_sin (mpc_ptr rop, mpc_srcptr op, mpc_rnd_t rnd)
       return;
     }
 
+  /* let op = a + i*b, then sin(op) = sin(a)*cosh(b) + i*cos(a)*sinh(b).
+
+     We use the following algorithm (same for the imaginary part),
+     with rounding to nearest for all operations, and working precision w:
+
+     (1) x = o(sin(a))
+     (2) y = o(cosh(b))
+     (3) r = o(x*y)     
+     then the error on r is at most 4 ulps, since we can write
+     r = sin(a)*cosh(b)*(1+t)^3 with |t| <= 2^(-w),
+     thus for w >= 2, r = sin(a)*cosh(b)*(1+4*t) with |t| <= 2^(-w),
+     thus the relative error is bounded by 4*2^(-w) <= 4*ulp(r).
+  */
+  
   prec = MPC_MAX_PREC(rop);
 
   mpfr_init2 (x, 2);
@@ -82,8 +137,8 @@ mpc_sin (mpc_ptr rop, mpc_srcptr op, mpc_rnd_t rnd)
                            MPFR_PREC(MPC_RE(rop)));
       if (ok) /* compute imaginary part */
         {
-	  mpfr_sinh (z, MPC_IM(op), GMP_RNDN);
-	  mpfr_mul (y, y, z, GMP_RNDN);
+          mpfr_sinh (z, MPC_IM(op), GMP_RNDN);
+          mpfr_mul (y, y, z, GMP_RNDN);
           ok = mpfr_can_round (y, prec - 2, GMP_RNDN, MPC_RND_IM(rnd),
                                MPFR_PREC(MPC_IM(rop)));
         }
--- tests/tsin.c
+++ tests/tsin.c
@@ -28,6 +28,231 @@ MA 02111-1307, USA. */
 #define TEST_FUNCTION mpc_sin
 #include "tgeneric.c"
 
+static void
+test_failed (mpc_t op, mpc_t get, mpc_t expected)
+{
+  printf ("mpc_sin(op) failed with ");
+  OUT (op);
+  printf ("     ");
+  OUT (get);
+  OUT (expected);
+  exit (1);
+}
+
+/* check special values as defined in C99 standard */
+static void
+special ()
+{
+  mpc_t z;
+  mpc_t s;
+  mpc_t c99;
+
+  mpc_init (z);
+  mpc_init (s);
+  mpc_init (c99);
+
+  mpfr_set_nan (MPC_RE (z));
+  /* sin(NaN +i*NaN) = NaN +i*NaN */
+  mpfr_set_nan (MPC_IM (z));
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, z);
+
+  /* sin(NaN +i*infinity) = NaN +/-i*infinity */
+  mpfr_set_inf (MPC_IM (z), +1);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_inf_p (MPC_IM (s)))
+    test_failed (z, s, z);
+
+  /* sin(NaN -i*infinity) = NaN -/+i*infinity */
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_inf_p (MPC_IM (s)))
+    test_failed (z, s, z);
+
+  /* sin(NaN +i*y) = NaN +i*NaN where 0<|y|<infinity */
+  mpfr_set_ui (MPC_IM (z), 1, GMP_RNDN);
+  mpfr_set_nan (MPC_RE (c99));
+  mpfr_set_nan (MPC_IM (c99));
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  /* sin(NaN +i*0) = NaN +/-i*0 */
+  mpfr_set_ui (MPC_IM (z), 0, GMP_RNDN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_zero_p (MPC_IM (s)))
+    test_failed (z, s, z);
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_zero_p (MPC_IM (s)))
+    test_failed (z, s, z);
+
+  /* sin(+0 +i*NaN) = +0 +i*NaN */
+  mpc_mul_i (z, z, +1, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_zero_p (MPC_RE (s)) || mpfr_signbit (MPC_RE (s)) != 0
+      || !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, z);
+
+  /* sin(-0 +i*NaN) = -0 +i*NaN */
+  mpc_neg (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_zero_p (MPC_RE (s)) || mpfr_signbit (MPC_RE (s)) == 0
+      || !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, z);
+
+  /* sin(x +i*NaN) = NaN +i*NaN where x!=0 */
+  mpfr_set_inf (MPC_RE (z), +1);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+  mpc_neg (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+  mpfr_set_ui (MPC_RE (z), +1, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+  mpc_neg (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) && !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  mpfr_set_inf (MPC_RE(z), -1);
+  /* sin(-infinity -i*infinity) = NaN -/+i*infinity */
+  mpfr_set_inf (MPC_IM(z), -1);
+  mpfr_set_nan (MPC_RE(c99));
+  mpfr_set_inf (MPC_IM(c99), -1);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_inf_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  /* sin(-infinity +i*infinity) = NaN +/-i*infinity */
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_conj (c99, c99, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_inf_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  /* sin(+infinity -i*infinity) = NaN -/+i*infinity */
+  mpc_neg (z, z, MPC_RNDNN);
+  mpc_neg (c99, c99, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_inf_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  /* sin(+infinity +i*infinity) = NaN +/-i*infinity */
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_conj (c99, c99, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_inf_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  /* sin(x +/-i*infinity) = infinity*(sin x -i*cos x) where 0<|x|<infinity */
+  mpfr_set_ui (MPC_RE (z), 1, GMP_RNDN);
+  mpfr_set_inf (MPC_RE (c99), +1);
+  mpfr_set_inf (MPC_IM (c99), +1);
+  mpc_sin (s, z, MPC_RNDUU);
+  if (mpc_cmp (s, c99) != 0)
+    test_failed (z, s, c99);
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDUU);
+  if (mpc_cmp (s, c99) != 0)
+    test_failed (z, s, c99);
+  mpfr_neg (MPC_RE (z), MPC_RE (z), GMP_RNDN);
+  mpfr_neg (MPC_RE (c99), MPC_RE (c99), GMP_RNDN);
+  mpc_sin (s, z, MPC_RNDUU);
+  if (mpc_cmp (s, c99) != 0)
+    test_failed (z, s, c99);
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDUU);
+  if (mpc_cmp (s, c99) != 0)
+    test_failed (z, s, c99);
+
+  /* sin(+0 -i*infinity) = +0 -i*infinity */
+  mpfr_set_ui (MPC_RE (z), 0, GMP_RNDN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (mpc_cmp (s, z) != 0)
+    test_failed (z, s, z);
+
+  /* sin(+0 +i*infinity) = +0 +i*infinity */
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (mpc_cmp (s, z) != 0)
+    test_failed (z, s, z);
+
+  /* sin(-0 -i*infinity) = -0 -i*infinity */
+  mpc_neg (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (mpc_cmp (s, z) != 0)
+    test_failed (z, s, z);
+
+  /* sin(-0 +i*infinity) = -0 +i*infinity */
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (mpc_cmp (s, z) != 0)
+    test_failed (z, s, z);
+
+  /* sin(-infinity +i*0) = NaN +/-i*0 */
+  mpfr_set_inf (MPC_RE (z), -1);
+  mpfr_set_ui (MPC_IM (z), 0, GMP_RNDN);
+  mpfr_set_nan (MPC_RE (c99));
+  mpfr_set_ui (MPC_IM (c99), 0, GMP_RNDN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_zero_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  /* sin(-infinity -i*0) = NaN -/+i*0 */
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_conj (c99, c99, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_zero_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  /* sin(+infinity +i*0) = NaN +/-i*0 */
+  mpc_neg (z, z, MPC_RNDNN);
+  mpc_neg (c99, c99, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_zero_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  /* sin(+infinity -i*0) = NaN -/+i*0 */
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_conj (c99, c99, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_zero_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  /* sin(+/-infinity +i*y) = NaN +i*NaN where 0<|y|<infinity*/
+  mpfr_set_ui (MPC_IM (z), 1, GMP_RNDN);
+  mpfr_set_nan (MPC_IM (c99));
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+  mpc_neg (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+  mpc_conj (z, z, MPC_RNDNN);
+  mpc_sin (s, z, MPC_RNDNN);
+  if (!mpfr_nan_p (MPC_RE (s)) || !mpfr_nan_p (MPC_IM (s)))
+    test_failed (z, s, c99);
+
+  mpc_clear (c99);
+  mpc_clear (s);
+  mpc_clear (z);
+}
+
 int
 main()
 {
@@ -39,7 +264,7 @@ main()
   mpc_init (z);
   mpfr_init (g);
 
-  for (prec = 2; prec <= 1000; prec++)
+  for (prec = 2; prec <= 1000; prec+=2)
     {
       mpc_set_prec (x, prec);
       mpc_set_prec (z, prec);
@@ -65,6 +290,7 @@ main()
 	}
     }
 
+  special ();
   tgeneric ();
 
   mpc_clear (x);
