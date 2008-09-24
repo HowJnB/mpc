a927d7e6ac6603cda73f2bbb905d2fc90b546157
Philippe Theveny <philippe.theveny@laposte.net>
2008-09-24 08:17:54 +0000

    tests/tlog.c: Put tests exp(log(z)) == z and log(exp(z)) == z into new function, use test_default_random instead of obsolete mpc_random, and speed up test.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@190 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- tests/tlog.c
+++ tests/tlog.c
@@ -29,68 +29,62 @@ MA 02111-1307, USA. */
 #define TEST_FUNCTION mpc_log
 #include "tgeneric_cc.c"
 
-int
-main (void)
+static void
+check_exp_log (void)
 {
    mpc_t z, z2, tmp;
    mpfr_t twopi;
    mp_prec_t prec;
 
-   test_start ();
-
-   tgeneric (2, 512, 7, 128);
-
-   mpc_init (z);
-   mpc_init (z2);
-   mpc_init (tmp);
+   mpc_init2 (z, 1000);
+   mpc_init2 (z2, 1000);
+   mpc_init2 (tmp, 4000);
    mpfr_init2 (twopi, 1000);
    mpfr_const_pi (twopi, GMP_RNDD);
    mpfr_div_2ui (twopi, twopi, 1, GMP_RNDN);
 
-   /* Test whether exp (log (z)) = z */
-   for (prec = 2; prec <= 1000; prec+=11)
+   for (prec = 2; prec <= 1000; prec = prec*1.1 + 1)
    {
       mpc_set_prec (z, prec);
       mpc_set_prec (z2, prec);
       mpc_set_prec (tmp, 4*prec);
 
-      /* The case of a purely imaginary number is treated below. */
-      do {
-         mpc_random (z);
-      } while (mpfr_zero_p (MPC_RE (z)));
+      /* Test whether exp (log (z)) = z, where z is no pure real
+         and no pure imaginary. */
+      test_default_random (z, 0, 10, 1);
+      if (mpfr_zero_p (MPC_RE (z)))
+        mpfr_set (MPC_RE (z), MPC_IM (z), GMP_RNDN);
+      else if  (mpfr_zero_p (MPC_IM (z)))
+        mpfr_set (MPC_IM (z), MPC_RE (z), GMP_RNDN);
+
       mpc_log (tmp, z, MPC_RNDNN);
       mpc_exp (z2, tmp, MPC_RNDNN);
 
       if (mpc_cmp (z, z2) != 0)
       {
-         printf ("Possible error in log; difference between z and z2=exp(log(z)):\n");
+         printf ("Possible error in log; difference between z and "\
+                 "z2=exp(log(z)):\n");
          OUT (z);
          OUT (tmp);
          OUT (z2);
          exit (1);
       }
-   }
 
 
-   /* Test whether log (exp (z)) = z for purely imaginary z; then exp (x) */
-   /* lies on the unit cercle, a critical case for the logarithm.         */
-   for (prec = 2; prec <= 1000; prec+=11)
-   {
-      mpc_set_prec (z, prec);
-      mpc_set_prec (z2, prec);
-      mpc_set_prec (tmp, 4*prec);
-
+      /* Test whether log (exp (z)) = z for purely imaginary z; then exp (x) */
+      /* lies on the unit cercle, a critical case for the logarithm.         */
       mpfr_set_ui (MPC_RE (z), 0, GMP_RNDN);
-      mpfr_random (MPC_IM (z));
+
       mpfr_remainder (MPC_IM (z), MPC_IM (z), twopi, GMP_RNDZ);
       mpc_exp (tmp, z, MPC_RNDNN);
       mpc_log (z2, tmp, MPC_RNDNN);
 
       /* There is a tiny real part, do not care if it si sufficiently small. */
-      if (   mpfr_cmp (MPC_IM (z), MPC_IM (z2)) != 0
+      if (mpfr_cmp (MPC_IM (z), MPC_IM (z2)) != 0
           || MPFR_EXP (MPC_RE (z)) > -4 * (mp_exp_t) prec)
       {
-         printf ("Possible error in purely imaginary log; difference between z and z2=log(exp(z)):\n");
+         printf ("Possible error in purely imaginary log; difference "\
+                 "between z and z2=log(exp(z)):\n");
          OUT (z);
          OUT (tmp);
          OUT (z2);
@@ -102,6 +96,15 @@ main (void)
    mpc_clear (z2);
    mpc_clear (tmp);
    mpfr_clear (twopi);
+}
+
+int
+main (void)
+{
+   test_start ();
+
+   check_exp_log ();
+   tgeneric (2, 512, 7, 128);
 
    test_end ();
 
