c24a66de5d89915a83425dce1d679db9139db9c9
Andreas Enge <andreas.enge@inria.fr>
2008-10-03 08:52:11 +0000

    web site augmented with development links
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@224 211d60ee-9f03-0410-a15a-8952a2c7a4e4

---
+++ web/content_development.php
@@ -0,0 +1,18 @@
+<h2>Development site</h2>
+
+Development is coordinated via an
+<a href="http://gforge.inria.fr/projects/mpc">INRIA Gforge project</a>.
+
+In particular, we offer
+<a href="http://gforge.inria.fr/scm/?group_id=131">anonymous
+subversion access</a>
+and access to the
+<a href="http://gforge.inria.fr/tracker/?atid=607&group_id=131&func=browse">bug</a>
+and
+<a href="http://gforge.inria.fr/tracker/?atid=610&group_id=131&func=browse">feature
+request</a>
+trackers.
+
+The
+<a href="http://lists.gforge.inria.fr/mailman/listinfo/mpc-discuss">mailing
+list</a> for user and developer discussions is hosted there as well.
