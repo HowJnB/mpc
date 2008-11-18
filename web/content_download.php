e896ee03164dc1da46bd60ffb48ced71b704abe7
Andreas Enge <andreas.enge@inria.fr>
2008-11-18 13:49:22 +0000

    Tagging release 0.5.1.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/tags/0.5.1@338 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- web/content_download.php
+++ web/content_download.php
@@ -1,5 +1,5 @@
 <div style="float:right">
-<a href="http://de.wikipedia.org/w/index.php?title=Bild:Blauer_Eisenhut_Dolomiten.jpg&amp;filetimestamp=20070808064748"><img alt="Aconitum" 
+<a href="http://de.wikipedia.org/w/index.php?title=Bild:Blauer_Eisenhut_Dolomiten.jpg&amp;filetimestamp=20070808064748"><img alt="Aconitum"
 src="http://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Blauer_Eisenhut_Dolomiten.jpg/300px-Blauer_Eisenhut_Dolomiten.jpg"/></a>
 </div>
 
@@ -7,27 +7,27 @@ src="http://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Blauer_Eisenhut_Do
 <ul>
 <li>
 <a href="http://gmplib.org/"><?php code ("Gmp"); ?></a>
-version 4.2.2 or higher
+version 4.2 or higher
 </li>
 <li>
 <a href="http://www.mpfr.org/"><?php code ("Mpfr"); ?></a>
-version 2.3.0 or higher
+version 2.3.1 or higher
 </li>
 </ul>
 
 <h2>Latest version</h2>
-<a href="download/mpc-0.5.tar.gz">Version 0.5</a> "Aconitum neomontanum"
+<a href="download/mpc-0.5.1.tar.gz">Version 0.5.1</a> "Aconitum neomontanum"
+
+<h2>Previous versions</h2>
+<a href="download/mpc-0.5.tar.gz">Version 0.5</a>
+
 
 <h2>Patches</h2>
 <p>
 The patches are applied as follows:<br/>
-<code>cd mpc-0.5</code><br/>
-<code>patch -N -p1 -Z -i path_to_patch_file</code></br>
+<code>cd mpc-0.5.1</code><br/>
+<code>patch -N -p1 -Z -i path_to_patch_file</code>
 </p>
-<ul>
-<li>
-<a href="download/configure.patch">Patch</a> for correctly
-recognising static <?php code("Gmp"); ?> and
-<?php code("Mpfr"); ?> libraries
-</li>
-</ul>
+<p>
+There are not yet any patches for the current version.
+</p>
