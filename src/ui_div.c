d161cadd6fe32bb3e315797769246fcce8fcfe64
Andreas Enge <andreas.enge@inria.fr>
2008-11-28 09:45:04 +0000

    revert to revision 359
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@361 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- src/ui_div.c
+++ src/ui_div.c
@@ -19,6 +19,7 @@ along with the MPC Library; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
+#include <limits.h>
 #include "gmp.h"
 #include "mpfr.h"
 #include "mpc.h"
@@ -37,7 +38,7 @@ mpc_ui_div (mpc_ptr a, unsigned long int b, mpc_srcptr c, mpc_rnd_t rnd)
       return MPC_INEX(inex, 0);
     }
 
-  mpc_init2 (bb, 8 * sizeof(unsigned long int));
+  mpc_init2 (bb, sizeof(unsigned long int) * CHAR_BIT);
 
   mpc_set_ui (bb, b, rnd); /* exact */
 
