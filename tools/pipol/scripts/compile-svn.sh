7aca411ae8544548a8c366862d51196eadbbbec2
Philippe Theveny <philippe.theveny@laposte.net>
2008-12-03 17:45:11 +0000

    tools/pipol/rc.ia64-linux-fedora, tools/pipol/rc.fedora, tools/pipol/rc.mac, tools/pipol/rc.redhat: run command files. tools/pipol/scripts/compile-svn.sh: improve printed message, most of them are removed when compilation works well. tools/pipol/nightly/multiopt: some compilation options are plateform dependant, use temp file which are not saved when everything works, add nightly build on MacOS and use different compilers.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@382 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- tools/pipol/scripts/compile-svn.sh
+++ tools/pipol/scripts/compile-svn.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 if [[ $# -lt 1 ]]; then
-	echo Usage: `basename $0` dir [options];
-	echo -n "Configure, compile, and check sources in \$PIPOL_WDIR/dir "
-	echo passing options to the compiler;
-	exit 1;
-	
-	else
-	echo \*; echo \* $0 $*; echo \*;
+    echo Usage: `basename $0` dir [options];
+    echo -n "Configure, compile, and check sources in \$PIPOL_WDIR/dir "
+    echo passing options to the compiler;
+    exit 1;
+else
+    echo \*
+    echo \* $0 $*
+    echo \*
 fi
 
 cd $PIPOL_WDIR/$1
 if [[ ! -f configure ]]; then
-	autoreconf -i || exit 1;
+	autoreconf -i || exit 1
 fi
 
 shift
@@ -20,8 +21,13 @@ if grep -q "CFLAGS=" <<<"$@"; then
 # and CFLAGS options manually
     CONFIGURE_OPTIONS=`sed -ne 's/\(.*\)\ *CFLAGS=\"\(.*\)\"\(.*\)/\1\3/p' <<<"$@"`
     CFLAGS_OPTIONS=`sed -ne 's/\(.*\)\ *CFLAGS=\"\(.*\)\"\(.*\)/CFLAGS=\2/p' <<<"$@"`
-    ./configure $CONFIGURE_OPTIONS "$CFLAGS_OPTIONS" && make && make check;
-
+    ./configure $CONFIGURE_OPTIONS "$CFLAGS_OPTIONS" 
 else
-    ./configure "$@" && make && make check;
+    ./configure "$@"
 fi
+if [ $? != 0 ]; then
+# configure failed
+    cat config.log
+    exit 1
+fi
+make && make check
