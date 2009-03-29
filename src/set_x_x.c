20a92b7dbfac7f986471eb3a40db7ef10ad02cfe
Paul Zimmermann <paul.zimmermann@inria.fr>
2009-03-29 08:08:49 +0000

    [set_x_x.c] updated copyright line
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@514 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- src/set_x_x.c
+++ src/set_x_x.c
@@ -1,7 +1,7 @@
 /* mpc_set_x_x -- Set complex number real and imaginary parts from parameters
    whose type is known by mpfr.
 
-Copyright (C) 2009 Philippe Th\'eveny
+Copyright (C) 2009 Philippe Th\'eveny, Paul Zimmermann
 
 This file is part of the MPC Library.
 
