3781fe911995f89276532ffbf473c7d9a251de47
Andreas Enge <andreas.enge@inria.fr>
2009-08-26 20:25:12 +0000

    change version number to 0.7-dev to get ready for prerelease tarball
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@666 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- src/get_version.c
+++ src/get_version.c
@@ -24,5 +24,5 @@ MA 02111-1307, USA. */
 const char *
 mpc_get_version (void)
 {
-  return "0.6.1-dev";
+  return "0.7-dev";
 }
--- src/mpc.h
+++ src/mpc.h
@@ -27,9 +27,9 @@ MA 02111-1307, USA. */
 
 /* Define MPC version number */
 #define MPC_VERSION_MAJOR 0
-#define MPC_VERSION_MINOR 6
-#define MPC_VERSION_PATCHLEVEL 1
-#define MPC_VERSION_STRING "0.6.1-dev"
+#define MPC_VERSION_MINOR 7
+#define MPC_VERSION_PATCHLEVEL 0
+#define MPC_VERSION_STRING "0.7-dev"
 
 /* Macros dealing with MPC VERSION */
 #define MPC_VERSION_NUM(a,b,c) (((a) << 16L) | ((b) << 8) | (c))
