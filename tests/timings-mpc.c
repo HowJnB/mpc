5d92740e46143e951d25557c477c09a42c739cdf
Paul Zimmermann <paul.zimmermann@inria.fr>
2011-02-10 14:44:39 +0000

    [timings-mpc.c] updated copyright notice
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@914 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- tests/timings-mpc.c
+++ tests/timings-mpc.c
@@ -1,22 +1,20 @@
 /* timings-mpc -- measure efficiency of MPC functions
 
-Copyright (C) INRIA, 2011
+Copyright (C) 2001, 2010, 2011 INRIA
+Copyright (C) 2001 Norbert Mueller (Univ. Trier, Germany)
 
-This file is part of the MPC Library.
-It was partly contributed by Norbert Mueller (Univ. Trier, Germany).
-
-The MPC Library is free software; you can redistribute it and/or modify
+This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation; either version 2.1 of the License, or (at your
 option) any later version.
 
-The MPC Library is distributed in the hope that it will be useful, but
+This program is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
 or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with the MPC Library; see the file COPYING.LIB.  If not, write to
+along with this program; see the file COPYING.LIB.  If not, write to
 the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 MA 02111-1307, USA. */
 
