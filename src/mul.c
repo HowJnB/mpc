add5135d043deef1d453b214e00ec60807c972a9
Andreas Enge <andreas.enge@inria.fr>
2011-02-15 19:56:09 +0000

    mul.c: updated copyright date
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@927 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- src/mul.c
+++ src/mul.c
@@ -1,6 +1,6 @@
 /* mpc_mul -- Multiply two complex numbers
 
-Copyright (C) INRIA, 2002, 2004, 2005, 2008, 2009, 2010
+Copyright (C) INRIA, 2002, 2004, 2005, 2008, 2009, 2010, 2011
 
 This file is part of the MPC Library.
 
