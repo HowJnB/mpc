3ba9de28f4499670436efb7755aa2afb938836d1
Andreas Enge <andreas.enge@inria.fr>
2011-02-23 14:16:53 +0000

    mpc-impl.h: added macro MPFR_OUT, analogous to MPC_OUT, to help debugging
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mpc/trunk@947 211d60ee-9f03-0410-a15a-8952a2c7a4e4

--- src/mpc-impl.h
+++ src/mpc-impl.h
@@ -110,13 +110,20 @@ MA 02111-1307, USA. */
    /* Consider as NaN all other numbers containing at least one NaN */
 
 
-#define MPC_OUT(x)                                                  \
+#define MPC_OUT(x)                                              \
 do {                                                            \
   printf (#x "[%lu,%lu]=", (unsigned long int) MPC_PREC_RE (x), \
       (unsigned long int) MPC_PREC_IM (x));                     \
   mpc_out_str (stdout, 2, 0, x, MPC_RNDNN);                     \
   printf ("\n");                                                \
 } while (0)
+#define MPFR_OUT(x)                                             \
+do {                                                            \
+  printf (#x "[%lu]=", (unsigned long int) mpfr_get_prec (x));  \
+  mpfr_out_str (stdout, 2, 0, x, GMP_RNDN);                     \
+  printf ("\n");                                                \
+} while (0)
+
 
 
 /*
