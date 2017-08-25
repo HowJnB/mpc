a3f939169482a4c502d276e1b3c814b0ea63f423
Paul Zimmermann <Paul.Zimmermann@inria.fr>
2017-08-25 09:32:18 +0200

    stdio.h is needed for MPC_ASSERT

--- src/rootofunity.c
+++ src/rootofunity.c
@@ -18,8 +18,8 @@ You should have received a copy of the GNU Lesser General Public License
 along with this program. If not, see http://www.gnu.org/licenses/ .
 */
 
+#include <stdio.h> /* for MPC_ASSERT */
 #include "mpc-impl.h"
-#include <assert.h>
 
 static unsigned long
 gcd (unsigned long a, unsigned long b)
